5660e6ad8840f039c07c501111d3a64801093469
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2023-08-23 13:50:33 +0530

    implementcta

--- src/pages/component/HomePage/HomePage.tsx
+++ src/pages/component/HomePage/HomePage.tsx
@@ -13,8 +13,8 @@ const HomePage: React.FC<HomePageProps> = ({ WebContent }) => {
     <>
       <Header Name={WebContent[0].Name} HeaderContent={WebContent[0].HeaderContent} HeaderDescription={WebContent[0].HeaderDescription} labels={WebContent[0].labels}/>
       <Stats stats={WebContent[0].stats}/> 
-      <Features/>
-      <Cta/>
+      <Features />
+      <Cta cta={WebContent[0].cta}/>
       <Footer />
     </>
   );
